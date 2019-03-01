47d80556ce07f5603c375c1a67d104a095f84229
rishabhgoel9797 <rishabhgoel9797@gmail.com>
2019-03-01 09:15:19 +0530

    Datatable added in teachers

--- resources/views/layouts/teacher_dashboard.blade.php
+++ resources/views/layouts/teacher_dashboard.blade.php
@@ -5,6 +5,8 @@
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
    <link href="{{asset('vendor/fontawesome-free/css/all.min.css')}}" rel="stylesheet" type="text/css">
     <link href="{{asset('css/sb-admin.css')}}" rel="stylesheet">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.css" rel="stylesheet">
+    <link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet">
     @yield('css')
 </head>
 
@@ -123,6 +125,8 @@
 <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
     <script src="{{asset('js/sb-admin.min.js')}}"></script>
 
+<script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
 @yield('script')
 </body>
 </html>
